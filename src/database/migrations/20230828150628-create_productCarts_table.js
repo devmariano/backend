efbe0cf1ccdd0d0d8fa94f596a795000b8e55d8f
Alexandre Mariano <86934710+devmariano@users.noreply.github.com>
2023-08-29 22:42:17 -0300

    criado o migration e model do productcarts

--- src/database/migrations/20230828150628-create_productCarts_table.js
+++ src/database/migrations/20230828150628-create_productCarts_table.js
@@ -3,20 +3,44 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.createTable('productCarts', { 
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    cart_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'carts',
+        key: 'cart_id'
+      }
+    },
+    product_id: {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'products',
+        key: 'product_id'
+      }
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    deleted_at: {
+      type: Sequelize.DATE
+    }
+    })
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('productCarts');
   }
 };
---
+++ src/models/ProductCart.js
@@ -0,0 +1,23 @@
+const { INTEGER, STRING, FLOAT } = require('sequelize')
+const { connection } = require('../database/connection')
+
+const ProductCart = connection.define('productCarts', {
+    cartId:  {
+        type: INTEGER,
+        references: {
+            model: {
+                tableName: 'carts'
+            }
+        }
+    },
+    productId:  {
+        type: INTEGER,
+        references: {
+            model: {
+                tableName: 'products'
+            }
+        }
+    }
+},{undescored: true, paranoid: true })
+
+module.exports = { ProductCart }
